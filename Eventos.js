4d1b8a06c922e4ef5cc9213856e93248595009ee
Vitor Oliveira <83097345+VitorDev01@users.noreply.github.com>
2022-02-03 16:09:28 -0300

    Add files via upload

---
+++ Eventos.js
@@ -0,0 +1,13 @@
+function somar() {
+            var tn1 = document.getElementById("txtn1")
+            
+            var tn2 = document.querySelector('input#txtn2')
+            
+            var res = document.getElementById("res")
+            
+            var n1 = Number(tn1.value)
+            var n2 = Number(tn2.value)
+            
+            var s = n1 + n2
+            document.write("A soma entre " , n1,  " e " ,n2 ," é : ", s)
+          }
